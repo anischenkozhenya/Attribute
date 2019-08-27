fc6d3c7bd2abe2871dbbc8885ca50ca6cc4750da
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-27 22:28:43 +0300

    +task2

---
+++ Task2/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    [Obsolete("Не стоит исспользовать этот класс, возможно он устарел", false)]
+    class MyClass
+    {        
+        [Obsolete("Метод помечен предупреждением", false)]
+        public void MethodWarning()
+        {
+            Console.WriteLine("Любой текст");
+        }
+        [Obsolete("Все правильно,но благодаря атрибуту выдается ошибка", true)]
+        public string Name { get; set; }
+        [Obsolete("Все правильно,но благодаря атрибуту выдается ошибка", true)]
+        public void MethodError()
+        {
+            Console.WriteLine("Какой-то текст");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance.MethodWarning();
+            //для проверки раскоментировать
+            //instance.MethodError();            
+        }
+    }
+}
